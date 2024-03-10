5422b3957c648c06237b14026af4bbd9cdf42c50
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-03-10 22:24:15 +0300

    refactor two sum

--- src/n0001/TwoSum.java
+++ src/n0001/TwoSum.java
@@ -9,7 +9,7 @@ public class TwoSum {
 
         int[] sumElements = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target && j != i) {
                     sumElements[0] = i;
                     sumElements[1] = j;
