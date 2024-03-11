ae65b49ae2517627819ee015ef4e77f8f4af87bf
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-03-11 22:36:47 +0300

    ReverseInteger with promt :(

--- src/n0007/ReverseInteger.java
+++ src/n0007/ReverseInteger.java
@@ -1,66 +1,32 @@
 package n0007;
 
-import static java.lang.Math.*;
-
 /**
  * 7. <a href="https://leetcode.com/problems/reverse-integer">Reverse Integer</a>
  */
 
 public class ReverseInteger {
 
+
+
     public int reverse(int x) {
-        int n = 2;
-        int max = (int) Math.pow(n, 31) - 1;
-        int min = (int) Math.pow(-n, 31);
 
-        if (x > max || x < min || x == 0) {
-            return 0;
-        }
+        int num = Math.abs(x);
 
-        int countNumbers = 0;
-        int delimiter = 1;
-        for (int i = Math.abs(x); i > 0; i /= 10) {
-            countNumbers++;
-            delimiter *= 10;
-        }
-        countNumbers--;
-        delimiter /= 10;
+        int rev = 0;
 
-        int reverseNumber =0;
-        int compositeNumber = 0;
-        int forTens = 1;
-        int xModule = Math.abs(x);
+        while (num != 0) {
+            int ld = num % 10;
 
-        for (int j = 0; j < countNumbers; j++, delimiter /= 10,forTens *= 10) {
 
-            compositeNumber += xModule / delimiter;
-            xModule %= delimiter;
-            reverseNumber += compositeNumber * forTens;
-            if (reverseNumber > max || reverseNumber < min) {
+            if (rev > (Integer.MAX_VALUE - ld) / 10) {
                 return 0;
             }
 
-            compositeNumber = 0;
+            rev = rev * 10 + ld;
+            num = num / 10;
         }
 
-        if (x < 0 ) {
-            return -reverseNumber;
-        }
-
-
-        return reverseNumber ;
-    }
-
-    public static void main(String[] args) {
-        int n = 2;
-        int i = 31;
-        ReverseInteger reverseInteger = new ReverseInteger();
-
-        System.out.println(reverseInteger.reverse(1534236466));
-
-        int adasads = Math.abs(-354);
-
-
+        return (x < 0) ? (-rev) : rev;
     }
 
 }
