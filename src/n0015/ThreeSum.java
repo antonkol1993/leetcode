edd20045f6ca40f22c2a06e24a6819efa8bc0479
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-03-13 22:54:18 +0300

    ThreeSum

---
+++ src/n0015/ThreeSum.java
@@ -0,0 +1,36 @@
+package n0015;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSum {
+    public List<Integer> variants = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> listList = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+
+        return listList;
+    }
+
+    public static void main(String[] args) {
+
+        ThreeSum threeSum = new ThreeSum();
+
+        int[] nums = {1};
+
+        System.out.println(threeSum.threeSum(nums));
+        System.out.println(threeSum.saddass);
+
+    }
+
+}
