da0c8234f919e1599eff2888520a0a361fcaab4e
WorkComputer <antonkol1993@gmail.com>
2024-03-27 16:40:15 +0300

    27/03 MedianOfTwoSortedArrays

---
+++ src/n0004/MedianOfTwoSortedArrays.java
@@ -0,0 +1,21 @@
+package n0004;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+/**
+ * 1.  <a href="https://leetcode.com/problems/median-of-two-sorted-arrays">MedianOfTwoSortedArrays</a>
+ */
+public class MedianOfTwoSortedArrays {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double median = 0;
+        List<Integer> allNumbs = new <TreeSet>();
+
+
+
+        return median;
+    }
+
+}
