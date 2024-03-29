d1880b98635b7c57556568e6168303cffa4341c1
WorkComputer <antonkol1993@gmail.com>
2024-03-29 18:38:10 +0300

    27/03 MedianOfTwoSortedArrays

--- src/n0004/MedianOfTwoSortedArrays.java
+++ src/n0004/MedianOfTwoSortedArrays.java
@@ -1,8 +1,6 @@
 package n0004;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * 1.  <a href="https://leetcode.com/problems/median-of-two-sorted-arrays">MedianOfTwoSortedArrays</a>
@@ -11,11 +9,44 @@ public class MedianOfTwoSortedArrays {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         double median = 0;
-        List<Integer> allNumbs = new <TreeSet>();
+        List<Integer> allarr = new ArrayList<>();
+
+        if (nums1.length > nums2.length) {
+            for (int i = 0; i < nums1.length; i++) {
 
 
+                allarr.add(nums1[i]);
+                if (i >= nums2.length) {
+                    continue;
+                }
+                allarr.add(nums2[i]);
+            }
+        }
+        else {
+                for (int i = 0; i < nums2.length; i++ ) {
 
+
+                    allarr.add(nums2[i]);
+                    if (i < nums1.length) {
+                        allarr.add(nums1[i]);
+                    }
+
+                }
+
+            }
+
+
+        TreeSet<Integer> aasdads = (TreeSet<Integer>) allarr;
+
+        System.out.println(allarr);
+        System.out.println();
+        System.out.println(aasdads);
         return median;
     }
 
+    public static void main(String[] args) {
+        MedianOfTwoSortedArrays medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
+        medianOfTwoSortedArrays.findMedianSortedArrays(new int[]{1,2,3,4,}, new int[]{5,9,8,6,7,});
+
+    }
 }
