5bc06c7961f3a7409c7c4a138e9b10b588126730
Anton Kolesnikov <antonkol1993@gmail.com>
2024-03-30 21:20:16 +0300

    jhjhbgh

--- src/n0020Cwithpromt/ValidParentheses.java
+++ src/n0020Cwithpromt/ValidParentheses.java
@@ -1,11 +1,13 @@
 package n0020Cwithpromt;
 
+import java.util.Stack;
+
 /**
  * 7. <a href="https://https://leetcode.com/problems/valid-parentheses">Valid Parentheses</a>
  */
 public class ValidParentheses {
     public boolean isValid(String s) {
-        do {
+        for (; ; ) {
             if (s.contains("()")) {
                 s = s.replace("()", "");
 
@@ -19,9 +21,41 @@ public class ValidParentheses {
                 return s.isEmpty();
             }
 
-        } while (true);
+        }
 
     }
 
+
+    public boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            switch (ch) {
+                case '(', '[', '{':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != ')') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '}') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
